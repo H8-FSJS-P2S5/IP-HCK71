c3c07bad7e705b0698eee8f2618817377bb0c7f9
joufando <joufando@gmail.com>
2024-06-14 00:47:01 +0700

    debugging

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ if (process.env.NODE_ENV !== "production") {
 
 const express = require("express");
 const app = express();
-
+const port = process.env.PORT || 3000;
 const cors = require("cors");
 app.use(cors());
 
